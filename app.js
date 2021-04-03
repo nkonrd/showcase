5c16db87ff657b3925f7c1ed4038a3923739c1de
nkonrd <nkozpvp@gmail.com>
2021-04-03 12:04:12 +0300

    refactor middleware

--- app.js
+++ app.js
@@ -1,37 +1,33 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const render = require('koa-ejs');
-const sqlite = require('sqlite3');
+const sqlite = require('sqlite-async');
 const fs = require('fs');
 const path = require('path');
 
 const port = 8080;
-const db = new sqlite.Database('movies.db');
-
 const app = new Koa();
 const router = new Router();
-
-let movies = []
-db.all('SELECT * FROM movies', (err, result) => {
-     result.forEach(movie => {movies.push(movie)});
-})
-db.close();
 
 app.use(require('koa-static')(__dirname));
 render(app, {
 	root: path.join(__dirname, 'views')
-});
+})
+
+app.use(async (ctx, next) => {
+	let db = await sqlite.open("movies.db")
+	let result = await db.all('SELECT * FROM movies');
+	this.movies = result
+	await next();
+})
 
 router.get('/', async ctx => {
-    await ctx.render('index', {movies: movies})
+    await ctx.render('index', {movies: this.movies});
 })
 
 router.get('/gallery', async ctx => {
-    await ctx.render('gallery', {movies: movies})
+    await ctx.render('gallery', {movies: this.movies});
 })
 
 app.use(router.routes()).use(router.allowedMethods());
-
-app.listen(port, () => {
-    console.log(`Listening on ${port}`)
-})
+app.listen(port, () => console.log("Listening..."));
